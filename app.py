ceb86da4625127beadca50f620de35a3d88ccdfa
Matheus <matheusvaz.data@gmail.com>
2025-05-04 23:57:56 -0300

    feat: route creation and 404 page UI - not found

--- app.py
+++ app.py
@@ -66,6 +66,9 @@ def pix_confirmation():
 def payment_pix_page(payment_id):
     payment = Payment.query.get(payment_id)
 
+    if not payment:
+        return render_template('404.html')
+
     if payment.paid:
         return render_template('confirmed_payment.html',
                                payment_id=payment.id,
@@ -81,6 +84,10 @@ def payment_pix_page(payment_id):
 @socketio.on('connect')
 def handle_connect():
     print('Client connected to server!')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected from server!')
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
